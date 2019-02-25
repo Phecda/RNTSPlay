afda1382b4a0a377dedceffec98fcf975c6bc3a0
Phecda <szp12345141@gmail.com>
2019-02-26 01:39:32 +0800

    remove react-native-dialogs

--- android/app/src/main/java/com/rntsplay/MainApplication.java
+++ android/app/src/main/java/com/rntsplay/MainApplication.java
@@ -6,7 +6,6 @@ import com.facebook.react.ReactApplication;
 import com.phecda.RNNativeDialogsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.rnfs.RNFSPackage;
-import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -31,7 +30,6 @@ public class MainApplication extends Application implements ReactApplication {
             new RNNativeDialogsPackage(),
             new VectorIconsPackage(),
             new RNFSPackage(),
-            new ReactNativeDialogsPackage(),
             new RNGestureHandlerPackage()
       );
     }
